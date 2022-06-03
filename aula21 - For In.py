5961dca16f9b645fe5afad7803f6665a30d48013
Cristiano Cerbaro <ccerbaro@gmail.com>
2022-06-03 11:40:40 -0300

    Upload aulas Python - Curso Udemy

--- aula21 - For In.py
+++ aula21 - For In.py
@@ -3,3 +3,24 @@
   Iterando strings com o for
   Função Range(start=0,stop,step=1
 """
+texto = 'Python'
+nova_string = ''
+
+# for n, letra in enumerate(texto):
+#     print(n,letra)
+
+for n in range(5, 10, 1):
+    print(n)
+
+for n in range(15, 10, -1):
+    print(n)
+
+for letra in texto:
+    if letra == 't':
+        nova_string = nova_string + letra.upper()
+    elif letra == 'h':
+        nova_string += letra.upper()
+    else:
+        nova_string += letra
+
+print(nova_string)
