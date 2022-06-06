35191d37999065ae0a4d2974424746e04eae7d7f
Cristiano Cerbaro <ccerbaro@gmail.com>
2022-06-06 11:25:31 -0300

    Upload Progresso aulas Python - Curso Udemy

--- aula22 - listas.py
+++ aula22 - listas.py
@@ -1,10 +1,30 @@
 """
 Listas em Python
+fatiamento
+append, insert, pop, del, clear, extend, +
+min, max
+range
 """
 
-lista = [1,2,3,4]
+l1 = [1,2,3]
+l2 = [4,5,6]
 
-#          0   1   2   3   4
-lista2 = ['A','B','C','D','E']
-#       -  5   4   3   2   1
-lista2[3] = 'mudando valor'
+l3 = l1 + l2  # concatena
+l1.extend(l2)
+
+print(l3)
+
+
+
+# lista = [1,2,3,4]
+#
+# #          0   1   2   3   4
+# lista2 = ['A','B','C','D','E']
+# #       -  5   4   3   2   1
+#
+# print(lista2[1:4])
+# print(lista2[:4])
+# print(lista2[::-1])
+#
+#
+# lista2[3] = 'mudando valor'
